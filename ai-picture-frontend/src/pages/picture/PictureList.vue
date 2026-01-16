73d5076964c4c4fbd5bb5a533592693aca2737de
LearnMore <2417948506@qq.com>
2026-01-16 16:25:49 +0800

    AI智能图库前端-7空间模块

--- ai-picture-frontend/src/pages/picture/PictureList.vue
+++ ai-picture-frontend/src/pages/picture/PictureList.vue
@@ -21,6 +21,14 @@
                 </a-flex>
               </template>
             </a-card-meta>
+            <template v-if="showOp" #actions>
+              <a-space @click="e => doEdit(picture, e)">
+                <edit-outlined />编辑
+              </a-space>
+              <a-space @click="e => doDelete(picture, e)">
+                <delete-outlined />删除
+              </a-space>
+            </template>
           </a-card>
         </a-list-item>
       </template>
@@ -30,15 +38,22 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import {deletePictureUsingPost} from "@/api/pictureController";
+import {message} from "ant-design-vue";
+import {DeleteOutlined, EditOutlined} from '@ant-design/icons-vue'
+
 
 interface Props {
   dataList?: API.PictureVO[]
   loading?: boolean
+  showOp?:boolean
+  onReload?:() => void
 }
 
 const props = withDefaults(defineProps<Props>(), {
   dataList: () => [],
   loading: false,
+  showOp: false,
 })
 
 // 跳转至图片详情
@@ -48,6 +63,36 @@ const doClickPicture = (picture) => {
     path: `/picture/${picture.id}`,
   })
 }
+
+// 编辑
+const doEdit = (picture, e) => {
+  e.stopPropagation()
+  router.push({
+    path: '/add_picture',
+    query: {
+      id: picture.id,
+      spaceId: picture.spaceId,
+    },
+  })
+}
+
+// 删除
+const doDelete = async (picture, e) => {
+  e.stopPropagation()
+  const id = picture.id
+  if (!id) {
+    return
+  }
+  const res = await deletePictureUsingPost({ id })
+  if (res.data.code === 0) {
+    message.success('删除成功')
+    // 让外层刷新
+    props?.onReload()
+  } else {
+    message.error('删除失败')
+  }
+}
+
 </script>
 
 <style scoped>
--- ai-picture-frontend/src/pages/space/SpaceDetailPage.vue
+++ ai-picture-frontend/src/pages/space/SpaceDetailPage.vue
@@ -10,7 +10,7 @@
     </a-space>
   </a-flex>
   <!-- 图片列表 -->
-  <PictureList :dataList="dataList" :loading="loading" />
+  <PictureList :dataList="dataList" :loading="loading" :showOp="true" :onReload="fetchData"/>
   <a-pagination style="text-align: right" v-model:current="searchParams.current" v-model:pageSize="searchParams.pageSize" :total="total" :show-total="() => `图片总数 ${total} / ${space.maxCount}`" @change="onPageChange"/>
 </template>
 
