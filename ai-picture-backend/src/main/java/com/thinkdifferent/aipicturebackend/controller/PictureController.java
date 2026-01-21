0ef386b241daa6bbd867f098888385bd0a5f0cd3
LearnMore <2417948506@qq.com>
2026-01-21 18:49:34 +0800

    7-空间模块：空间相关代码生成:上传图片去除权限

--- ai-picture-backend/src/main/java/com/thinkdifferent/aipicturebackend/controller/PictureController.java
+++ ai-picture-backend/src/main/java/com/thinkdifferent/aipicturebackend/controller/PictureController.java
@@ -73,7 +73,7 @@ public class PictureController {
      * 上传图片（可重新上传）
      */
     @PostMapping("/upload")
-    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    //@AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<PictureVO> uploadPicture(@RequestPart("file") MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, HttpServletRequest request) {
         User loginUser = userService.getLoginUser(request);
         PictureVO pictureVO = pictureService.uploadPicture(multipartFile, pictureUploadRequest, loginUser);
