0ddf449b944b736895bd0cfbd785b30d380c71a5
LearnMore <2417948506@qq.com>
2026-04-16 14:34:57 +0800

    AI智能云图库后端-引入JWT修改

--- ai-picture-backend/src/main/java/com/thinkdifferent/aipicturebackend/jwt/JwtAuthenticationFilter.java
+++ ai-picture-backend/src/main/java/com/thinkdifferent/aipicturebackend/jwt/JwtAuthenticationFilter.java
@@ -2,23 +2,17 @@ package com.thinkdifferent.aipicturebackend.jwt;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @Slf4j
 @Component
