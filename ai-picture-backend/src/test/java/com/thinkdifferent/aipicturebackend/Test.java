f3eb089a585bb2aca79f1c8b799b56b07802bd19
LearnMore <2417948506@qq.com>
2026-04-24 10:50:46 +0800

    测试类

--- ai-picture-backend/src/test/java/com/thinkdifferent/aipicturebackend/Test.java
+++ ai-picture-backend/src/test/java/com/thinkdifferent/aipicturebackend/Test.java
@@ -10,7 +10,9 @@ public class Test {
     public void testdemo1() {
         // 测试字符串包含各种空格
         String test = "Hello\u00A0World\u200B!\u3000\u00A0\u200B";
+        String test1 = "你好呀";
         System.out.println("原始字符串长度: " + test.length());
+        System.out.println("原始字符串长度: " + test1.length());
         System.out.println("原始: '" + test + "'");
         // 测试各种方法
         System.out.println("\n使用trim(): '" + test.trim() + "'"+"字符串长度："+test.trim().length());
